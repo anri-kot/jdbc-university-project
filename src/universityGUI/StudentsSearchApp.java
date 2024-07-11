c9c06fab15359720cf4c66640016aea7d930a154
theAuthor <me@realemail.com>
2024-07-11 17:49:40 -0300

    students data added to interface

--- src/universityGUI/StudentsSearchApp.java
+++ src/universityGUI/StudentsSearchApp.java
@@ -1,6 +1,7 @@
 package universityGUI;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,30 +35,48 @@ public class StudentsSearchApp extends JFrame {
         }
 
         // ---- frame properties ----
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout(FlowLayout.LEADING));
-        setMinimumSize(new Dimension(500, 476));
+        setMinimumSize(new Dimension(600, 500));
+        // setResizable(false);
         setTitle("Search Students");
 
         // ---- frame objects ----
+
+        // objects container
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(new EmptyBorder(3, 3, 3, 3));
+        mainPanel.setLayout(new BorderLayout());
+
         // top panel
         JPanel topSrcPanel = new JPanel();
-        topSrcPanel.setPreferredSize(new Dimension(470, 40));
+        topSrcPanel.setMinimumSize(new Dimension(470, 40));
         topSrcPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+
         // top panel label
         JLabel label = new JLabel();
         label.setText("Search by name: ");
         label.setHorizontalAlignment(JLabel.LEFT);
         topSrcPanel.add(label);
+
         // top panel text field
         JTextField searchTextField = new JTextField();
         searchTextField.setPreferredSize(new Dimension(250, 25));
         topSrcPanel.add(searchTextField);
+
+        // table
+        JTable table = new JTable();
+
         // top panel search button
         JButton searchButton = new JButton("Search");
         searchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // search action
+
+                // -- search action --
+
+                // A. search for student name inputed and print student(s) data if textField is
+                // not empty
+                // B. print all data if textField is empty
                 try {
                     String name = searchTextField.getText();
                     List<Students> students = null;
@@ -67,9 +86,17 @@ public class StudentsSearchApp extends JFrame {
                     } else {
                         students = dao.getAllStudents();
                     }
-                    for (Students temp : students) {
-                        System.out.println(temp);
-                    }
+
+                    // create model and update table
+                    StudentsTableModel model = new StudentsTableModel(students);
+                    table.setModel(model);
+
+                    /*
+                     * for (Students temp : students) {
+                     * System.out.println(temp);
+                     * }
+                     */
+
                 } catch (Exception exc) {
                     JOptionPane.showMessageDialog(StudentsSearchApp.this, "Error: " + exc);
                 }
@@ -78,7 +105,10 @@ public class StudentsSearchApp extends JFrame {
         topSrcPanel.add(searchButton);
 
         // ---- add frame objects ----
-        add(topSrcPanel);
+
+        add(mainPanel); // objects container
+        mainPanel.add(topSrcPanel, BorderLayout.NORTH);
+        mainPanel.add(new JScrollPane(table), BorderLayout.CENTER);
 
     }
 
---
+++ src/universityGUI/StudentsTableModel.java
@@ -0,0 +1,59 @@
+package universityGUI;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import universityCore.Students;
+
+public class StudentsTableModel extends AbstractTableModel {
+    private final static int ST_MATR_COL = 0;
+    private final static int ST_NAME_COL = 1;
+    private final static int ST_ADDRESS_COL = 2;
+    private final static int ST_PHONE_COL = 3;
+    private final static int ST_SSN_COL = 4;
+
+    private String[] columnNames = { "ID", "Name", "Address", "Phone", "SSN" };
+    private List<Students> students;
+
+    public StudentsTableModel(List<Students> theStudents) {
+        students = theStudents;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public int getRowCount() {
+        return students.size();
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        Students tempStudents = students.get(row);
+
+        switch (col) {
+            case ST_MATR_COL:
+                return tempStudents.getMatr();
+            case ST_NAME_COL:
+                return tempStudents.getName();
+            case ST_ADDRESS_COL:
+                return tempStudents.getAddress();
+            case ST_PHONE_COL:
+                return tempStudents.getPhone();
+            case ST_SSN_COL:
+                return tempStudents.getSsn();
+            default:
+                return tempStudents.getName();
+        }
+    }
+
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+    }
+}
